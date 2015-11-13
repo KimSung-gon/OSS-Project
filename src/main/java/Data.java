b280e91441ba8f3344a2281f36a96048c029f546
DongHoon <dhdnzk0304@gmail.com>
2015-11-13 16:21:33 +0900

    학생 개개인 데이터 정보를 담는 노드

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+
 public class Data {
 
+    Data ( int studentID, String name, String major ) {
+        this.studentID = studentID;
+        this.name = name;
+        this.major = major;
+    }
+
+    Data ( int studentID, String name, String major, int age ) {
+
+        this(studentID, name, major );
+        this.age = age;
+    }
+
+
+    int studentID;
+    String name;
+    int age;
+    String major;
+    ArrayList<String> takingClass;
 
 
 }
+
+
 // TODO 학생 정보 저장
 // TODO 학생 정보 수정
 // TODO 학생 정보 삭제
