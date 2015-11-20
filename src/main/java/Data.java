e0f2c98045dec543e5801ec506c32b2b7ce92ea9
DongHoon <dhdnzk0304@gmail.com>
2015-11-20 16:03:20 +0900

    데이터 클래스 일부 수정 및 구현

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Data {
 
@@ -13,17 +14,44 @@ public class Data {
     }
 
     public void saveData(){
-        ArrayList<String> takeclass = new ArrayList<String>();
-        takeclass.add("Math");
-        takeclass.add("English");
-        StudentList.getInstance().slist.add(new Student(20113300, 24, "MS", "CS", takeclass));
+
+        Scanner sc = new Scanner( System.in );
+
+        int studentID;
+        int age;
+        String name;
+        String major;
+
+        System.out.print("이름 : ");
+        name = sc.next();
+
+        System.out.print("학번 : ");
+        studentID = sc.nextInt();
+
+        System.out.print("나이 : ");
+        age = sc.nextInt();
+
+        System.out.print("전공 : ");
+        major = sc.next();
+
+//        TODO 수강중인 과목 함수 호출해서 저장하기
+//        ArrayList<String> takeclass = new ArrayList<String>();
+//        takeclass.add("Math");
+//        takeclass.add("English");
+//        StudentList.getInstance().slist.add(new Student(studentID, age, name, major, takeclass));
     }
 
     public void modifyData(){
 
+       // TODO 학번 입력받아서 학생 정보 받아온 뒤에 원하는 정보 수정
+
     }
 
     public void removeData(){
+       int idx = 0;
+
+//        TODO 지울 데이터 계산해서 매개변수로 전달
+        StudentList.getInstance().slist.remove( idx );
 
     }
 
