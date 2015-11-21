97f5a85a221c196e6f82a7156cbee9f2fc732a8e
“Asy11” <dbsaudtlr04@hanmail.net>
2015-11-21 19:03:16 +0900

    텍스트 파일을 읽고 쓰는 메서드 추가 및 removeData, saveData 메서드 수정

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -13,10 +13,22 @@ public class Data {
         return datIinstance;
     }
 
+    // txt 파일에 있는 데이터를 읽어 slist에 저장시켜주는 메소드
+    public void readData(){
+
+    }
+
+    // txt 파일에 slist의 데이터를 써주는 메소드
+    public void saveToTxt(){
+
+    }
+
+    // slist에 학생 정보를 저장하는 매소드
     public void saveData(){
 
         Scanner sc = new Scanner( System.in );
 
+        ArrayList<String> takeclass = new ArrayList<String>();
         int studentID;
         int age;
         String name;
@@ -34,11 +46,17 @@ public class Data {
         System.out.print("전공 : ");
         major = sc.next();
 
-//        TODO 수강중인 과목 함수 호출해서 저장하기
-//        ArrayList<String> takeclass = new ArrayList<String>();
-//        takeclass.add("Math");
-//        takeclass.add("English");
-//        StudentList.getInstance().slist.add(new Student(studentID, age, name, major, takeclass));
+        System.out.println("수강중인 과목 : ");
+        while(true){
+            System.out.println("과목 입력을 멈추시려면 1을 입력해 주세요.");
+            String lecture = sc.next();
+            if(lecture == "1")
+                break;
+            else
+                takeclass.add(lecture);
+        }
+
+        StudentList.getInstance().slist.add(new Student(studentID, age, name, major, takeclass));
     }
 
     public void modifyData(){
@@ -48,10 +66,24 @@ public class Data {
     }
 
     public void removeData(){
-       int idx = 0;
 
-//        TODO 지울 데이터 계산해서 매개변수로 전달
-        StudentList.getInstance().slist.remove( idx );
+        Scanner sc = new Scanner( System.in );
+        int studentId;
+        int i;
+
+        System.out.print("지우고자 하는 학생의 학번을 입력해 주세요 : ");
+
+        studentId = sc.nextInt();
+
+
+        for(i = 0 ; i < StudentList.getInstance().slist.size() ; i++ ){
+            if(StudentList.getInstance().slist.get(i).studentID == studentId)
+                StudentList.getInstance().slist.remove( studentId );
+
+        }
+
+        if(i == StudentList.getInstance().slist.size()+1)
+            System.out.println("삭제할 학생이 없습니다.");
 
     }
 
