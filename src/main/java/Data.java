9497f67d7e47c2f38ca11d11d50fe10724a6954d
DongHoon <dhdnzk0304@gmail.com>
2015-11-24 17:21:50 +0900

    파일 입출력 구현중

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,13 +17,59 @@ public class Data {
     }
 
     // txt 파일에 있는 데이터를 읽어 slist에 저장시켜주는 메소드
-    public void readData(){
+    public static void loadDataFromFile() throws IOException {
+        Reader read = new FileReader("studentList.txt");
+        BufferedReader bReader = new BufferedReader(read);
 
+        String tmp = bReader.readLine();
+        while ( tmp != null && tmp.compareTo( "#" ) == 0 ) {
+            if ( tmp.compareTo( "#" )  == 0 ) {
+                String studentID = bReader.readLine();
+                String age = bReader.readLine();
+                String name = bReader.readLine();
+                String major = bReader.readLine();
+                ArrayList<String> takingClass = new ArrayList<String>();
+
+                String temp = bReader.readLine();
+                while ( temp != null && temp.compareTo( "#" ) != 0 ) {
+                    takingClass.add(temp);
+                    temp = bReader.readLine();
+                }
+                Student newStudent = new Student(Integer.parseInt(studentID), Integer.parseInt(age), name, major, takingClass);
+                StudentList.getInstance().slist.add(newStudent);
+                tmp = bReader.readLine();
+            }
+            else
+                break;
+        }
     }
 
     // txt 파일에 slist의 데이터를 써주는 메소드
-    public void saveToTxt(){
+    public static void saveDataToFile() throws IOException {
+        Writer write = new FileWriter( "studentList.txt" );
+        BufferedWriter bWriter = new BufferedWriter( write );
 
+        if (StudentList.getInstance().slist != null ) {
+            bWriter.write("#\n");
+
+            int i = 0;
+            while (StudentList.getInstance().slist != null) {
+
+                bWriter.write(String.valueOf(StudentList.getInstance().slist.get(i).studentID) + "\n" );
+                bWriter.write(String.valueOf(StudentList.getInstance().slist.get(i).age) + "\n" );
+                bWriter.write(StudentList.getInstance().slist.get(i).name + "\n" );
+                bWriter.write(StudentList.getInstance().slist.get(i).major + "\n" );
+                bWriter.flush();
+
+                int j = 1;
+                while (StudentList.getInstance().slist.get(j) != null) {
+                    bWriter.write(StudentList.getInstance().slist.get(i).takingClass.get(j) + "\n");
+                    j++;
+                }
+                bWriter.flush();
+                i++;
+            }
+        }
     }
 
     // slist에 학생 정보를 저장하는 매소드
@@ -34,6 +83,7 @@ public class Data {
         String name;
         String major;
 
+        System.out.println( "========== 1. 데이터 추가 ===========");
         System.out.print("이름 : ");
         name = sc.next();
 
@@ -46,17 +96,22 @@ public class Data {
         System.out.print("전공 : ");
         major = sc.next();
 
-        System.out.println("수강중인 과목 : ");
+        System.out.println("과목 입력을 멈추시려면 \"stop\"을 입력해 주세요.");
+        int i = 1;
         while(true){
-            System.out.println("과목 입력을 멈추시려면 1을 입력해 주세요.");
+            System.out.printf("수강중인 과목 %d : ", i );
             String lecture = sc.next();
-            if(lecture == "1")
+            if( lecture.compareTo( "stop" ) == 0 ) {
                 break;
-            else
+            }
+            else {
                 takeclass.add(lecture);
+                i++;
+            }
         }
-
         StudentList.getInstance().slist.add(new Student(studentID, age, name, major, takeclass));
+        System.out.printf("현재 수강중인 과목(%d개) 저장되었습니다", i - 1 );
+
     }
 
     public void modifyData(){
