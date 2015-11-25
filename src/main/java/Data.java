431e60a123c2630f67532473fb6da1a53bbc557e
KimSung-gon <gonni27@naver.com>
2015-11-25 23:08:02 +0900

    modifyData

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -122,7 +122,74 @@ public class Data {
     public void modifyData(){
 
        // TODO 학번 입력받아서 학생 정보 받아온 뒤에 원하는 정보 수정
+        Scanner sc = new Scanner(System.in);
+        int studentId;
+        int changeDataNumber;
+        System.out.print("변경 할 학번을 입력하세요 : ");
+        studentId = sc.nextInt();
 
+        while (true) {
+            for (int i = 0; i < StudentList.getInstance().slist.size(); i++) {
+                if (StudentList.getInstance().slist.get(i).studentID == studentId) {
+                    System.out.println("====================");
+                    System.out.println("데이터 수정");
+                    System.out.println("1.나이");
+                    System.out.println("2.이름");
+                    System.out.println("3.전공");
+                    System.out.println("4.수강중인 수업");
+                    System.out.println("5.나가기");
+                    System.out.print("수정할 데이터 번호를 입력하세요 : ");
+
+                    changeDataNumber = sc.nextInt();
+
+                    if (changeDataNumber == 1) {
+                        System.out.println("현재 나이 : " + StudentList.getInstance().slist.get(i).age);
+                        System.out.print("변경할 나이를 입력하세요 : ");
+                        int ageToChange = sc.nextInt();
+                        StudentList.getInstance().slist.get(i).age = ageToChange;
+                    }
+
+                    if (changeDataNumber == 2) {
+                        System.out.println("현재 이름 : " + StudentList.getInstance().slist.get(i).name);
+                        System.out.print("변경할 이름을 입력하세요 : ");
+                        String nameToChange = sc.next();
+                        StudentList.getInstance().slist.get(i).name = nameToChange;
+                    }
+
+                    if (changeDataNumber == 3) {
+                        System.out.println("현재 전공 : " + StudentList.getInstance().slist.get(i).major);
+                        System.out.print("변경할 전공을 입력하세요 : ");
+                        String majorToChange = sc.next();
+                        StudentList.getInstance().slist.get(i).major = majorToChange;
+                    }
+
+                    if (changeDataNumber == 4) {
+                        while (true) {
+                            System.out.println("현재 수강중인 과목 : ");
+                            for (int j = 0; j < StudentList.getInstance().slist.get(i).takingClass.size(); j++)
+                                System.out.println(StudentList.getInstance().slist.get(i).takingClass.get(j));
+                            System.out.println("1.추가    2.삭제    3.나가기");
+                            System.out.print("실행 할 번호를 입력하세요 : ");
+                            int addOrRemoveOrModify = sc.nextInt();
+                            if (addOrRemoveOrModify == 1) {
+                                System.out.print("추가 할 과목을 입력하세요 : ");
+                                String addSubject = sc.next();
+                                StudentList.getInstance().slist.get(i).takingClass.add(addSubject);
+                            }
+                            if (addOrRemoveOrModify == 2) {
+                                System.out.print("삭제 할 과목을 입력하세요 : ");
+                                String removeSubject = sc.next();
+                                StudentList.getInstance().slist.get(i).takingClass.remove(removeSubject);
+                            }
+                            if (addOrRemoveOrModify == 3)
+                                return;
+                        }
+                    }
+                    if (changeDataNumber == 5)
+                        return;
+                }
+            }
+        }
     }
 
     public void removeData() throws IOException {
