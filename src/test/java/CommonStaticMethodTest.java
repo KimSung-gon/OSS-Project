08033e6044b8b17cb412ab1447d4c15bbbdd211e
DongHoon <dhdnzk0304@gmail.com>
2015-12-04 16:36:37 +0900

    테스트 케이스 추가

--- src/test/java/CommonStaticMethodTest.java
+++ src/test/java/CommonStaticMethodTest.java
@@ -8,9 +8,33 @@ public class CommonStaticMethodTest {
     @Test
     public void searchStudentIdxNumberByStudentID () {
 
-//        ArrayList<Student>slist = new ArrayList<Student>();
+//        ArrayList<Student> slist = new ArrayList<Student>();
 //        StudentList testList = new StudentList( slist );
 
+
+    }
+
+    @Test
+    public void isNumberic () {
+        String testStr1 = "123";
+        String testStr2 = "a123";
+        String testStr3 = "1a23";
+        String testStr4 = "12a3";
+        String testStr5 = "가나다";
+        String testStr6 = "112i2234";
+        String testStr7 = "112630k";
+        String testStr8 = "number";
+        String testStr9 = " 123";
+
+        TestCase.assertTrue( CommonStaticMethod.isNumeric(testStr1) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr2) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr3) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr4) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr5) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr6) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr7) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr8) );
+        TestCase.assertFalse( CommonStaticMethod.isNumeric(testStr9) );
 
     }
 
