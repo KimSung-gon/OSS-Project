97b30b53190481c2468014ce308d3f7f60786fbc
DongHoon <dhdnzk0304@gmail.com>
2015-12-07 01:05:56 +0900

    import 최적화, searchDataOfYear(테스트 메소드) 실패하는 이슈 해결

--- src/test/java/SearchingTest.java
+++ src/test/java/SearchingTest.java
@@ -1,40 +1,50 @@
 import junit.framework.TestCase;
-import org.junit.Test;
-
-import java.sql.Connection;
 import java.util.ArrayList;
+import org.junit.Test;
 
 public class SearchingTest {
 
     ArrayList<Student> testsList = new ArrayList<Student>();
 
     @Test
-    public void setUp(){
+    public void setUp() {
+
         ArrayList<String> testClass1 = new ArrayList<String>();
-        testClass1.add("모바일프로그래밍");
-        testClass1.add("선형대수");
-        testClass1.add("응용통계학");
-        testClass1.add("이산수학");
-        testsList.add(new Student(20110001, 24, "일번학생", "컴퓨터공학" , testClass1));
+        testClass1.add( "모바일프로그래밍" );
+        testClass1.add( "선형대수" );
+        testClass1.add( "응용통계학" );
+        testClass1.add( "이산수학" );
+        testsList.add( new Student( 20110001, 24, "일번학생", "컴퓨터공학" , testClass1 ) );
 
         ArrayList<String> testClass2 = new ArrayList<String>();
-        testClass2.add("미분과적분");
-        testClass2.add("선형대수");
-        testClass2.add("응용통계학");
-        testsList.add(new Student(20110002, 24, "이번학생", "수학" , testClass2));
+        testClass2.add( "미분과적분" );
+        testClass2.add( "선형대수" );
+        testClass2.add( "응용통계학" );
+        testsList.add(new Student( 20110002, 24, "이번학생", "수학" , testClass2 ) );
 
         ArrayList<String> testClass3 = new ArrayList<String>();
-        testClass3.add("모바일프로그래밍");
-        testClass3.add("선형대수");
-        testClass3.add("응용통계학");
-        testClass3.add("이산수학");
-        testsList.add(new Student(20110003, 24, "삼번학생", "컴퓨터공학" , testClass3));
+        testClass3.add( "모바일프로그래밍" );
+        testClass3.add( "선형대수" );
+        testClass3.add( "응용통계학" );
+        testClass3.add( "이산수학" );
+        testsList.add( new Student( 20110003, 24, "삼번학생", "컴퓨터공학" , testClass3 ) );
+
+        ArrayList<String> testClass4 = new ArrayList<String>();
+        testClass3.add( "모바일프로그래밍" );
+        testClass3.add( "선형대수" );
+        testClass3.add( "응용통계학" );
+        testClass3.add( "이산수학" );
+        testsList.add( new Student( 20100003, 24, "사번학생", "컴퓨터공학" , testClass3 ) );
     }
 
     @Test
-    public void serchDataOfYearTest(){
+    public void serchDataOfYear() {
 
-        TestCase.assertEquals(0, Searching.searchDataOfYear(0000, testsList));
+        setUp();
+        TestCase.assertEquals( 0, Searching.searchDataOfYear( 2000, testsList ) );
+        TestCase.assertEquals( 1, Searching.searchDataOfYear( 2010, testsList ) );
+        TestCase.assertEquals( 1, Searching.searchDataOfYear( 2011, testsList ) );
+        TestCase.assertEquals( 0, Searching.searchDataOfYear( 2012, testsList ) );
+
     }
-
 }
