d69cb5ddf99404697bc96cadc0ea8397c795f879
“Asy11” <dbsaudtlr04@hanmail.net>
2015-12-07 11:53:15 +0900

    Serching TestCase 완료

--- src/main/java/Searching.java
+++ src/main/java/Searching.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 
 public class Searching extends CommonStaticMethod {
 
-    public static int searchDataOfStudent(int studentIdx) {
+    public static int searchDataOfStudent(int studentIdx, ArrayList<Student> slist) {
         if (studentIdx == -1) {
             System.out.println("조회되는 학번이 없습니다.");
             return 0;
-        }
-        else
-            showdata(StudentList.getInstance().slist.get(studentIdx));
+        } else
+            showdata(slist.get(studentIdx));
         return 1;
     }                  // 학번을 통한 정보 검색 (성공 : 1 , 실패 : 0)
 
@@ -35,30 +34,30 @@ public class Searching extends CommonStaticMethod {
         System.out.println("===========================");
     }              // 학생 개인정보 출력
 
-    public static int searchStudentDataOfSubject(String subject) {
+    public static int searchStudentDataOfSubject(String subject, ArrayList<Student> slist) {
 
         int count = 0;
-        for (int i = 0; i < StudentList.getInstance().slist.size(); i++)
-            for (int j = 0; j < StudentList.getInstance().slist.get(i).takingClass.size(); j++)
-                if (StudentList.getInstance().slist.get(i).takingClass.get(j).compareTo(subject) == 0) {
-                    showStudentIdAndName(StudentList.getInstance().slist.get(i));
+        for (int i = 0; i < slist.size(); i++)
+            for (int j = 0; j < slist.get(i).takingClass.size(); j++)
+                if (slist.get(i).takingClass.get(j).compareTo(subject) == 0) {
+                    showStudentIdAndName(slist.get(i));
                     count++;
                 }
-        if(count == 0)
+        if (count == 0)
             return 0;
         else
             return 1;
     }           // 과목을 통한 정보 검색 (성공 : 1 , 실패 : 0)
 
-    public static int searchStudentDataOfMajor(String major) {
+    public static int searchStudentDataOfMajor(String major, ArrayList<Student> slist) {
 
         int count = 0;
-        for (int i = 0; i < StudentList.getInstance().slist.size(); i++)
-            if (StudentList.getInstance().slist.get(i).major.compareTo(major) == 0) {
-                showStudentIdAndName(StudentList.getInstance().slist.get(i));
+        for (int i = 0; i < slist.size(); i++)
+            if (slist.get(i).major.compareTo(major) == 0) {
+                showStudentIdAndName(slist.get(i));
                 count++;
             }
-        if(count == 0)
+        if (count == 0)
             return 0;
         else
             return 1;
@@ -68,11 +67,11 @@ public class Searching extends CommonStaticMethod {
 
         int count = 0;
         for (int i = 0; i < slist.size(); i++)
-            if ( slist.get(i).studentID / 10000 == selectedYear ) {
+            if (slist.get(i).studentID / 10000 == selectedYear) {
                 showStudentIdAndName(slist.get(i));
                 count++;
             }
-        if(count == 0)
+        if (count == 0)
             return 0;
         else
             return 1;
--- src/test/java/SearchingTest.java
+++ src/test/java/SearchingTest.java
@@ -1,5 +1,7 @@
 import junit.framework.TestCase;
+
 import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class SearchingTest {
@@ -10,41 +12,75 @@ public class SearchingTest {
     public void setUp() {
 
         ArrayList<String> testClass1 = new ArrayList<String>();
-        testClass1.add( "모바일프로그래밍" );
-        testClass1.add( "선형대수" );
-        testClass1.add( "응용통계학" );
-        testClass1.add( "이산수학" );
-        testsList.add( new Student( 20110001, 24, "일번학생", "컴퓨터공학" , testClass1 ) );
+        testClass1.add("모바일프로그래밍");
+        testClass1.add("선형대수");
+        testClass1.add("응용통계학");
+        testClass1.add("이산수학");
+        testsList.add(new Student(20110001, 24, "일번학생", "컴퓨터공학", testClass1));
 
         ArrayList<String> testClass2 = new ArrayList<String>();
-        testClass2.add( "미분과적분" );
-        testClass2.add( "선형대수" );
-        testClass2.add( "응용통계학" );
-        testsList.add(new Student( 20110002, 24, "이번학생", "수학" , testClass2 ) );
+        testClass2.add("미분과적분");
+        testClass2.add("선형대수");
+        testClass2.add("응용통계학");
+        testsList.add(new Student(20110002, 24, "이번학생", "수학", testClass2));
 
         ArrayList<String> testClass3 = new ArrayList<String>();
-        testClass3.add( "모바일프로그래밍" );
-        testClass3.add( "선형대수" );
-        testClass3.add( "응용통계학" );
-        testClass3.add( "이산수학" );
-        testsList.add( new Student( 20110003, 24, "삼번학생", "컴퓨터공학" , testClass3 ) );
+        testClass3.add("모바일프로그래밍");
+        testClass3.add("선형대수");
+        testClass3.add("응용통계학");
+        testClass3.add("이산수학");
+        testsList.add(new Student(20110003, 24, "삼번학생", "컴퓨터공학", testClass3));
 
         ArrayList<String> testClass4 = new ArrayList<String>();
-        testClass3.add( "모바일프로그래밍" );
-        testClass3.add( "선형대수" );
-        testClass3.add( "응용통계학" );
-        testClass3.add( "이산수학" );
-        testsList.add( new Student( 20100003, 24, "사번학생", "컴퓨터공학" , testClass3 ) );
+        testClass3.add("모바일프로그래밍");
+        testClass3.add("선형대수");
+        testClass3.add("응용통계학");
+        testClass3.add("이산수학");
+        testsList.add(new Student(20100003, 24, "사번학생", "컴퓨터공학", testClass3));
+    }
+
+    @Test
+    public void searchDataOfStudentTest() {
+        setUp();
+        TestCase.assertEquals(1, Searching.searchDataOfStudent(0, testsList));
+        TestCase.assertEquals(1, Searching.searchDataOfStudent(1, testsList));
+        TestCase.assertEquals(1, Searching.searchDataOfStudent(2, testsList));
+        TestCase.assertEquals(0, Searching.searchDataOfStudent(-1, testsList));
+    }
+
+    @Test
+    public void searchStudentDataOfSubjectTest() {
+
+        setUp();
+        TestCase.assertEquals(1, Searching.searchStudentDataOfSubject("이산수학", testsList));
+        TestCase.assertEquals(1, Searching.searchStudentDataOfSubject("모바일프로그래밍", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfSubject("영어", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfSubject("틀린입력", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfSubject("afdsaewfawef", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfSubject("!@#%$!@%#!@#$", testsList));
+    }
+
+    @Test
+    public void searchStudentDataOfMajorTest() {
+
+        setUp();
+        TestCase.assertEquals(1, Searching.searchStudentDataOfMajor("컴퓨터공학", testsList));
+        TestCase.assertEquals(1, Searching.searchStudentDataOfMajor("수학", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfMajor("틀린학과입력", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfMajor("adsfaewf", testsList));
+        TestCase.assertEquals(0, Searching.searchStudentDataOfMajor("!@#$!^!$#%", testsList));
+
     }
 
     @Test
     public void serchDataOfYear() {
 
         setUp();
-        TestCase.assertEquals( 0, Searching.searchDataOfYear( 2000, testsList ) );
-        TestCase.assertEquals( 1, Searching.searchDataOfYear( 2010, testsList ) );
-        TestCase.assertEquals( 1, Searching.searchDataOfYear( 2011, testsList ) );
-        TestCase.assertEquals( 0, Searching.searchDataOfYear( 2012, testsList ) );
-
+        TestCase.assertEquals(0, Searching.searchDataOfYear(2000, testsList));
+        TestCase.assertEquals(1, Searching.searchDataOfYear(2010, testsList));
+        TestCase.assertEquals(1, Searching.searchDataOfYear(2011, testsList));
+        TestCase.assertEquals(0, Searching.searchDataOfYear(2012, testsList));
+        TestCase.assertEquals(0, Searching.searchDataOfYear(12341245, testsList));
     }
+
 }
