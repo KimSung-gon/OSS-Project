9f20c9d556ef43a7dbd603cfba6f9fd394818f1e
Asy11 <dbsaudtlr04@hanmail.net>
2015-12-09 16:07:54 +0900

    줄맞춤

--- src/main/java/CommonStaticMethod.java
+++ src/main/java/CommonStaticMethod.java
@@ -22,8 +22,8 @@ public class CommonStaticMethod {
     protected final static void showStudentData(Student s) {
         System.out.printf("학번 : %d\n", s.studentID);
         System.out.printf("이름 : %s\n", s.name);
-        System.out.printf("나이 : %d\n", s.age );
-        System.out.printf("전공 : %s\n", s.major );
+        System.out.printf("나이 : %d\n", s.age);
+        System.out.printf("전공 : %s\n", s.major);
         s.showTakingClass();
     }             // 학생 개인정보 출력
 
@@ -51,7 +51,7 @@ public class CommonStaticMethod {
         return sc.nextInt();
     }                                 // 정수형을 입력받아 리턴
 
-    protected final static void returnMenu(){
+    protected final static void returnMenu() {
         System.out.print("메뉴로 돌아가려면 아무 키나 입력하세요 : ");
         inputString();
     }                               // 메뉴로 돌아가기
