7bc0db15af357b033c7e0ca4e9345d84edfe590d
“Asy11” <dbsaudtlr04@hanmail.net>
2015-12-11 15:39:38 +0900

    Manager 스위치 구문 안의 코드들 밖으로 뺌

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -21,109 +21,26 @@ public class Manager {
 
 
             try {
-                Scanner sc = new Scanner(System.in);
                 showMainMenu();
 
-                int select = sc.nextInt();
+                int select = CommonStaticMethod.inputInt();
 
 
                 switch (select) {
                     case 1: {
-                        ArrayList<String> takeclass = new ArrayList<String>();
-                        int studentID;
-                        int age;
-                        String name;
-                        String major;
-
-                        System.out.println("========== 1. 데이터 추가 ===========");
-                        System.out.print("이름 : ");
-                        name = CommonStaticMethod.inputString();
-
-                        System.out.print("학번 : ");
-                        studentID = CommonStaticMethod.inputInt();
-
-                        System.out.print("나이 : ");
-                        age = CommonStaticMethod.inputInt();
-
-                        System.out.print("전공 : ");
-                        major = CommonStaticMethod.inputString();
-
-                        System.out.println("과목 입력을 멈추시려면 \"stop\"을 입력해 주세요.");
-                        int i = 1;
-
-                        while (true) {
-                            System.out.printf("수강중인 과목 %d : ", i);
-                            String lecture = CommonStaticMethod.inputString();
-                            if (lecture.compareTo("stop") == 0) {
-                                break;
-                            } else {
-                                takeclass.add(lecture);
-                                i++;
-                            }
-                        }
-
-                        System.out.printf("현재 수강중인 과목(%d개) 저장되었습니다", i - 1);
-
-                        Data.getInstance().saveStudentData(studentID, age, name, major, takeclass, StudentList.getInstance().slist);
+                        saveData();
                         break;
                     }
                     case 2: {
-
-                        System.out.print("변경 할 학번을 입력하세요 : ");
-                        int studentId = CommonStaticMethod.askStudentID();
-
-                        int idx = Data.getInstance().modifyStudentData(studentId, StudentList.getInstance().slist);
-
-
-                        while (true) {
-                            Data.showWayToModify();
-                            int changeDataNumber = CommonStaticMethod.inputInt();
-                            int ageToChange = -1;
-                            String nameToChange = "";
-                            String majorToChange = "";
-
-                            if (changeDataNumber == 1) {
-                                System.out.println("현재 나이 : " + StudentList.getInstance().slist.get(idx).age);
-                                System.out.print("변경할 나이를 입력하세요 : ");
-                                ageToChange = CommonStaticMethod.inputInt();
-                            } else if (changeDataNumber == 2) {
-                                System.out.println("현재 이름 : " + StudentList.getInstance().slist.get(idx).name);
-                                System.out.print("변경할 이름을 입력하세요 : ");
-                                nameToChange = CommonStaticMethod.inputString();
-                            } else if (changeDataNumber == 3) {
-                                System.out.println("현재 전공 : " + StudentList.getInstance().slist.get(idx).major);
-                                System.out.print("변경할 전공을 입력하세요 : ");
-                                majorToChange = CommonStaticMethod.inputString();
-                            }
-
-                            int close = Data.getInstance().modifyStudentDataInfo(idx, changeDataNumber, ageToChange, nameToChange, majorToChange, StudentList.getInstance().slist);
-
-                            if (close == 0)
-                                break;
-                        }
+                        modifyData();
                         break;
                     }
                     case 3: {
-
-                        System.out.print("지우고자 하는 학생의 학번을 입력해 주세요 : ");
-                        int studentId = CommonStaticMethod.askStudentID();
-                        int idx = CommonStaticMethod.searchStudentIdxNumberByStudentID(studentId, StudentList.getInstance().slist);
-                        if (idx != -1) {
-                            Data.getInstance().removeStudentData(idx, StudentList.getInstance().slist);
-                            System.out.printf("삭제하시겠습니까?( y/n ) : ");
-                            boolean choice = Data.choiceSaveDataOrNot(CommonStaticMethod.inputString());
-                            Data.getInstance().removeStudentDataConfirm(idx, choice, StudentList.getInstance().slist);
-                        }
-                        CommonStaticMethod.returnMenu();
+                        deleteData();
                         break;
                     }
                     case 4: {
-                        System.out.println("찾고자 하는 학생의 학번을 입력해 주세요 : ");
-                        int studentId = CommonStaticMethod.askStudentID();
-                        int studentIdx = CommonStaticMethod.searchStudentIdxNumberByStudentID(studentId, StudentList.getInstance().slist);
-
-                        Searching.searchDataOfStudent(studentIdx, StudentList.getInstance().slist);
-                        CommonStaticMethod.returnMenu();
+                        findDataByStudentId();
                         break;
                     }
                     case 5:
@@ -131,30 +48,15 @@ public class Manager {
                         CommonStaticMethod.returnMenu();
                         break;
                     case 6: {
-                        System.out.print("찾고자 하는 과목명을 입력해 주세요 : ");
-                        String subject = CommonStaticMethod.inputString();
-                        int success = Searching.searchStudentDataOfSubject(subject, StudentList.getInstance().slist);
-                        if (success == 0)
-                            System.out.println("해당하는과목이 없습니다. 과목명을 확인해주세요.");
-                        CommonStaticMethod.returnMenu();
+                        findDataByClass();
                         break;
                     }
                     case 7: {
-                        System.out.print("찾고자 하는 전공명을 입력해 주세요 : ");
-                        String major = CommonStaticMethod.inputString();
-                        int success = Searching.searchStudentDataOfMajor(major, StudentList.getInstance().slist);
-                        if (success == 0)
-                            System.out.println("해당하는 과가 없습니다. 전공명을 확인해주세요.");
-                        CommonStaticMethod.returnMenu();
+                        findDataByMajor();
                         break;
                     }
                     case 8: {
-                        System.out.print("찾고자 하는 입학년도를 네자리로 입력해 주세요 : ");
-                        int year = Integer.parseInt(CommonStaticMethod.inputStringNumber());
-                        int success = Searching.searchDataOfYear(year, StudentList.getInstance().slist);
-                        if (success == 0)
-                            System.out.println("해당하는 입학년도가 없습니다. 입학년도를 확인해주세요.");
-                        CommonStaticMethod.returnMenu();
+                        findDataByYear();
                         break;
                     }
                     case 9:
@@ -164,10 +66,10 @@ public class Manager {
                 if (select == 9)
                     return;
             } catch (InputMismatchException ex) {
-                System.out.println("인풋 미스매치");
+                System.out.println("잘못된 입력입니다.");
 
             } catch (NumberFormatException ex) {
-                System.out.println("넘버 포맷 익셉션");
+                System.out.println("잘못된 입력입니다.");
             }
 
         }
@@ -191,5 +93,135 @@ public class Manager {
         System.out.printf("9. 프로그램 종료\n");
         System.out.printf("==================================\n");
         System.out.printf("선택 : ");
+    }
+
+    public static void saveData(){
+        ArrayList<String> takeclass = new ArrayList<String>();
+        int studentID;
+        int age;
+        String name;
+        String major;
+
+        System.out.println("========== 1. 데이터 추가 ===========");
+        System.out.print("이름 : ");
+        name = CommonStaticMethod.inputString();
+
+        System.out.print("학번 : ");
+        studentID = CommonStaticMethod.inputInt();
+
+        System.out.print("나이 : ");
+        age = CommonStaticMethod.inputInt();
+
+        System.out.print("전공 : ");
+        major = CommonStaticMethod.inputString();
+
+        System.out.println("과목 입력을 멈추시려면 \"stop\"을 입력해 주세요.");
+        int i = 1;
+
+        while (true) {
+            System.out.printf("수강중인 과목 %d : ", i);
+            String lecture = CommonStaticMethod.inputString();
+            if (lecture.compareTo("stop") == 0) {
+                break;
+            } else {
+                takeclass.add(lecture);
+                i++;
+            }
+        }
+
+        System.out.printf("현재 수강중인 과목(%d개) 저장되었습니다", i - 1);
+
+        Data.getInstance().saveStudentData(studentID, age, name, major, takeclass, StudentList.getInstance().slist);
+    }
+
+    public static void modifyData(){
+
+        System.out.print("변경 할 학번을 입력하세요 : ");
+        int studentId = CommonStaticMethod.askStudentID();
+
+        int idx = Data.getInstance().modifyStudentData(studentId, StudentList.getInstance().slist);
+
+
+        while (true) {
+            Data.showWayToModify();
+            int changeDataNumber = CommonStaticMethod.inputInt();
+            int ageToChange = -1;
+            String nameToChange = "";
+            String majorToChange = "";
+
+            if (changeDataNumber == 1) {
+                System.out.println("현재 나이 : " + StudentList.getInstance().slist.get(idx).age);
+                System.out.print("변경할 나이를 입력하세요 : ");
+                ageToChange = CommonStaticMethod.inputInt();
+            } else if (changeDataNumber == 2) {
+                System.out.println("현재 이름 : " + StudentList.getInstance().slist.get(idx).name);
+                System.out.print("변경할 이름을 입력하세요 : ");
+                nameToChange = CommonStaticMethod.inputString();
+            } else if (changeDataNumber == 3) {
+                System.out.println("현재 전공 : " + StudentList.getInstance().slist.get(idx).major);
+                System.out.print("변경할 전공을 입력하세요 : ");
+                majorToChange = CommonStaticMethod.inputString();
+            }
+
+            int close = Data.getInstance().modifyStudentDataInfo(idx, changeDataNumber, ageToChange, nameToChange, majorToChange, StudentList.getInstance().slist);
+
+            if (close == 0)
+                break;
+        }
+
+    }
+
+    public static void deleteData(){
+
+        System.out.print("지우고자 하는 학생의 학번을 입력해 주세요 : ");
+        int studentId = CommonStaticMethod.askStudentID();
+        int idx = CommonStaticMethod.searchStudentIdxNumberByStudentID(studentId, StudentList.getInstance().slist);
+        if (idx != -1) {
+            Data.getInstance().removeStudentData(idx, StudentList.getInstance().slist);
+            System.out.printf("삭제하시겠습니까?( y/n ) : ");
+            boolean choice = Data.choiceSaveDataOrNot(CommonStaticMethod.inputString());
+            Data.getInstance().removeStudentDataConfirm(idx, choice, StudentList.getInstance().slist);
+        }
+        CommonStaticMethod.returnMenu();
+
+    }
+
+    public static void findDataByStudentId(){
+        System.out.println("찾고자 하는 학생의 학번을 입력해 주세요 : ");
+        int studentId = CommonStaticMethod.askStudentID();
+        int studentIdx = CommonStaticMethod.searchStudentIdxNumberByStudentID(studentId, StudentList.getInstance().slist);
+
+        Searching.searchDataOfStudent(studentIdx, StudentList.getInstance().slist);
+        CommonStaticMethod.returnMenu();
+
+    }
+
+    public static void findDataByClass(){
+        System.out.print("찾고자 하는 과목명을 입력해 주세요 : ");
+        String subject = CommonStaticMethod.inputString();
+        int success = Searching.searchStudentDataOfSubject(subject, StudentList.getInstance().slist);
+        if (success == 0)
+            System.out.println("해당하는과목이 없습니다. 과목명을 확인해주세요.");
+        CommonStaticMethod.returnMenu();
+
+    }
+
+    public static void findDataByMajor(){
+        System.out.print("찾고자 하는 전공명을 입력해 주세요 : ");
+        String major = CommonStaticMethod.inputString();
+        int success = Searching.searchStudentDataOfMajor(major, StudentList.getInstance().slist);
+        if (success == 0)
+            System.out.println("해당하는 과가 없습니다. 전공명을 확인해주세요.");
+        CommonStaticMethod.returnMenu();
+
+    }
+
+    public static void findDataByYear(){
+        System.out.print("찾고자 하는 입학년도를 네자리로 입력해 주세요 : ");
+        int year = Integer.parseInt(CommonStaticMethod.inputStringNumber());
+        int success = Searching.searchDataOfYear(year, StudentList.getInstance().slist);
+        if (success == 0)
+            System.out.println("해당하는 입학년도가 없습니다. 입학년도를 확인해주세요.");
+        CommonStaticMethod.returnMenu();
     }
 }
